a9f45b7ee49f559e295ff5ad18b87d95ab298361
전은형 <ooooo5h91@gmail.com>
2022-02-06 19:54:11 +0900

    네이버 주식 현재가 1 종목 추출하기

--- 01_네이버_주식현재가_크롤링/01_데이터추출.py
+++ 01_네이버_주식현재가_크롤링/01_데이터추출.py
@@ -7,4 +7,4 @@ html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 price = soup.select_one("#_nowVal").text
 price = price.replace(',', '')  # 문자열을 숫자로 바꾸기 위해서 ,를 제거해줌
-print(price)
+print(price) 
