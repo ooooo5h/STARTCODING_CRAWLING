78f6c703a1174925fc07a141ef3f0c00d28432d8
전은형 <ooooo5h91@gmail.com>
2022-02-06 19:57:49 +0900

    종목코드리스트를 만들어서 현재가 크롤링하기

--- 01_네이버_주식현재가_크롤링/01_데이터추출.py
+++ 01_네이버_주식현재가_크롤링/01_데이터추출.py
@@ -1,10 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.naver.com/item/sise.naver?code=000660"
-response = requests.get(url)
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-price = soup.select_one("#_nowVal").text
-price = price.replace(',', '')  # 문자열을 숫자로 바꾸기 위해서 ,를 제거해줌
-print(price) 
+# 종목 코드 리스트
+codes = [
+    '095610',
+    '006360',
+    '114090',    
+]
+
+for code in codes:
+    url = f"https://finance.naver.com/item/sise.naver?code={code}"
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    price = soup.select_one("#_nowVal").text
+    price = price.replace(',', '')  # 문자열을 숫자로 바꾸기 위해서 ,를 제거해줌
+    print(price) 
